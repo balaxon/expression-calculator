aa442a7bf305288c3eaf60ceda2d885656990932
balaxon <balahanov2209@gmail.com>
2020-03-21 23:06:09 +0300

    feat: All done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 function eval() {
-    // Do not use eval!!!
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let arr= expr.replace(/\s/g,"").split("");
+    console.log(arguments);
+    error_new(arr);
+
+    function error_new(arr) {
+        let a = 0;
+        for(let i = 0; i<arr.length;i++){
+            if(arr[i]==="("){
+                a++;
+            }
+            if(arr[i]===")"){
+                a--;
+            }
+        }
+        if(a!==0){
+            throw new Error("ExpressionError: Brackets must be paired.");
+        }
+
+        for(let i = 0; i<arr.length;i++){
+            if(arr[i]==="/"){
+                if(arr[i+1] === "0")
+                throw new Error("TypeError: Division by zero.")
+            }
+        }
+    }
+    itog = new Function("return " + expr.replace(/\s/g,""));
+    return itog();
 }
 
 module.exports = {
